505ba9d72e293c05893eca2e86a475b13fbbadb8
Lucio Studer Ferreira <42048382+luciostuder@users.noreply.github.com>
2026-03-20 17:09:32 +0000

    urls

--- project/urls.py
+++ project/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
+urlpatterns_ = [
     path('admin/', admin.site.urls),
     path('', include("obras.urls")),
 ]
