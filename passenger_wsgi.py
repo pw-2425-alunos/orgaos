a5cd96031b5a1fe4b572664c78e504b5a67f03ed
Lucio Studer Ferreira <42048382+luciostuder@users.noreply.github.com>
2026-04-23 12:29:08 +0000

    updates

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,8 +1,11 @@
-import os
 import sys
+import os
 
-sys.path.insert(0, os.path.dirname(__file__))
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+project_home = '/home/obmdatab/repositories/orgaos-2'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'project.settings'
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
