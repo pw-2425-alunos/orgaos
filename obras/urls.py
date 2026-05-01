acfa6078cb193a5e06a9777875ba1ae16a1197a7
Lucio Studer Ferreira <42048382+luciostuder@users.noreply.github.com>
2026-05-01 10:36:16 +0000

    url

--- obras/urls.py
+++ obras/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.landing_view, name='landing'),
+    path('web/', views.landing_view),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('pesquisa/', views.pesquisa_view, name='pesquisa'),
