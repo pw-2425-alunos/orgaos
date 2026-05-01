77b9ce8f0a62c7cfedbf066d2955b8a462652dee
luciostuder <lucio.studer@gmail.com>
2026-05-01 23:21:41 +0100

    never cache

--- obras/urls.py
+++ obras/urls.py
@@ -1,20 +1,21 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.views.decorators.cache import never_cache
 from . import views
 
 urlpatterns = [
-    path('', views.landing_view, name='landing'),
-    path('web/', views.landing_view),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-    path('pesquisa/', views.pesquisa_view, name='pesquisa'),
-    path('obras/novo', views.criar_obra_view, name="criar_obra"),
-    path('compositores/', views.compositores_view, name='compositores'),
-    path('compositores/novo', views.criar_compositor_view, name='criar_compositor'),
-    path('compositores/<int:id>', views.compositor_view, name="compositor"),
-    path('compositores/<int:id>/editar', views.editar_compositor_view, name='editar_compositor'),
-    path('obras/<int:id>', views.obra_view, name="obra"),
-    path('obras/<int:id>/editar', views.editar_obra_view, name="editar_obra"),
-    path('sobre/', views.sobre_view, name="sobre"),
+    path('', never_cache(views.landing_view), name='landing'),
+    path('web/', never_cache(views.landing_view)),
+    path('login/', never_cache(views.login_view), name='login'),
+    path('logout/', never_cache(views.logout_view), name='logout'),
+    path('pesquisa/', never_cache(views.pesquisa_view), name='pesquisa'),
+    path('obras/novo', never_cache(views.criar_obra_view), name="criar_obra"),
+    path('compositores/', never_cache(views.compositores_view), name='compositores'),
+    path('compositores/novo', never_cache(views.criar_compositor_view), name='criar_compositor'),
+    path('compositores/<int:id>', never_cache(views.compositor_view), name="compositor"),
+    path('compositores/<int:id>/editar', never_cache(views.editar_compositor_view), name='editar_compositor'),
+    path('obras/<int:id>', never_cache(views.obra_view), name="obra"),
+    path('obras/<int:id>/editar', never_cache(views.editar_obra_view), name="editar_obra"),
+    path('sobre/', never_cache(views.sobre_view), name="sobre"),
 ]
